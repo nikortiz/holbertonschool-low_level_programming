eb110a3fa8549a21ada8a3defcaa228ec1a23675
nikortiz <nikortiz28@gmail.com>
2023-10-18 12:37:40 -0400

    outputs the alphabet in lower and upper case

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,11 +7,13 @@
 int main(void) /*outputs the alphabet in lower and uppercase until Z and z*/
 {
 	char alphabet;
+	char uAlpha;
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+	for (uAlpha = 'A'; uAlpha <= 'Z'; uAlpha++)
 	{
 		putchar(alphabet);
+		putchar(uAlpha);
 	}
 		putchar('\n');
 		return (0);
