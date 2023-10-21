e5030a0080b5b13b9fe7f4b94b31bba675cbf02b
nikortiz <nikortiz28@gmail.com>
2023-10-21 19:55:09 -0400

    checks for lowercase

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *int _islower - checks for lowercase char
+ *
+ * Return 1 if lowercase 0 if else
+ */
+int _islower(int k)
+{
+	if (k >= 97 && k <= 122)
+{
+	return (1);
+}
+	return (0);
+	}
