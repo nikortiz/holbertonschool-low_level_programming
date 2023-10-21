ea0b6e3fca77fe3fcbf9c0e32a38df67ca373c0b
nikortiz <nikortiz28@gmail.com>
2023-10-21 11:20:30 -0400

    prints the alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - print the alphabet
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+		_putchar('\n');
+}
