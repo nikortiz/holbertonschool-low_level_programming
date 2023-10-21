38b350c8d8daf37056ce9b271e8571fa609d591d
nikortiz <nikortiz28@gmail.com>
2023-10-21 12:02:50 -0400

    outputs the abcs x10

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,16 +5,17 @@
  *
  * Return: Always 0
  */
-void print_alphabet_x10(void)
-{
+void print_alphabet_x10(void){
+
 	char a;
 	int b;
-
+	
+	for (b = 1; b <= 10; b++)
 	{
-	for (a = 'a'; a <= 'z'; a++)
-	{
-		_putchar(a);
-	}
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
 		_putchar('\n');
 	}
 }
