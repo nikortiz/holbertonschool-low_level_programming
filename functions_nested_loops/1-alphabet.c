48aeb0de623cd93f0f3b30284ddc8a989f3303a2
nikortiz <nikortiz28@gmail.com>
2023-10-21 11:51:14 -0400

    outputs the abcs x10

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet - outputs the abcs.
+ * print_alphabet - outputs the abcs 10x.
  *
  * Return: Always 0
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char a;
+	int b;
 
+	{
 	for (a = 'a'; a <= 'z'; a++)
 	{
 		_putchar(a);
 	}
 		_putchar('\n');
+	}
 }
---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print alphabet x10 - outputs the abcs 10x
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void);
+{
+	char a;
+	int b;
+
+	for (b = 1; b <= 10; b++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+			_putchar('\n');
+		}
+	}
+}
+
+
