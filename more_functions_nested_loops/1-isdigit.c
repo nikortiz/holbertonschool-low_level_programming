6ccfcd47ce241fba5fd36fea803a1d44e0ea6b2c
nikortiz <nikortiz28@gmail.com>
2023-10-24 13:02:04 -0400

    checks for a digit 0-9

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -4,8 +4,8 @@
  * _isdigit - checks for digit 0-9
  * @c: char value evaluated
  *
- * return 1 if the character is a digit,
- * return 0 otherwise
+ * return (1) - if the character is a digit,
+ * return (0) - otherwise
  */
 
 int _isdigit(int c)
